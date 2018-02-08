c6a3a4c85ae9531a9b49599cdfb2c16bc1ab4ff0
cmbuhler <cmbuhler@ualberta.ca>
2018-02-07 20:17:17 -0700

    Adding lab 5 work

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
@@ -1,8 +1,13 @@
 package ca.ualberta.cs.lonelytwitter;
+import java.util.Date;
 
 public class NormalTweet extends Tweet {
 
     public NormalTweet(String message) {
+        super(message);
+    }
+
+    public NormalTweet(String message, Date date) {
         super(message);
     }
 
